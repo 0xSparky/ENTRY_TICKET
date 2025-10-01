2bb296732fd4a25ede42ebf585cff908ee31b9df
ingenjor-siam <md_siam.ahmed0005@stud.hkr.se>
2025-10-01 04:43:23 +0200

    Update S01T04.py

--- S01T04.py
+++ S01T04.py
@@ -1,32 +1,28 @@
 title = input("Enter the title: ")
-print("")
 shopping_list = ""
+
 while True:
+    print()
     print("1. Add an item")
     print("2. Print currently added items")
     print("3. Finished")
-    # adding a new menu
     print("R. Reset shopping list")
+    print()
 
-    choice = input(
-        "Enter your choice :"
-    ).upper()  # changed to make all input upper case
+    choice = input("Enter your choice :")
+    
     if choice == "1":
         item = input("Enter an item:")
         shopping_list += item + "\n"
 
     elif choice == "2":
         print("Currently added items:")
-        # if shopping_list.strip()=='':
-        #    print('(no items yet)')
-        # else:
         print(shopping_list)
 
     elif choice == "3":
         print("_" * 20)
         print(title.center(20))
         print("_" * 20)
-
         print(shopping_list)
         break
 
@@ -34,9 +30,10 @@ while True:
         confirm = input("Are you sure (y/n)?")
         if confirm == "y":
             shopping_list = ""
-            print("Shopping list has been reset.\n")
+            print("Shopping list has been reset.)
         else:
-            print("Reset cancelled.\n")
+            print("Reset cancelled.")
     else:
-        print("Error: Invalid choice, please try again.\n")
+        print("Error: Invalid choice, please try again.")
+
  
