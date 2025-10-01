48ff930c6dda77d7013bae2e6e2368d7281044f9
ingenjor-siam <md_siam.ahmed0005@stud.hkr.se>
2025-10-01 04:40:33 +0200

    Update S01T03.py

--- S01T03.py
+++ S01T03.py
@@ -1,13 +1,14 @@
 title = input('Enter the title:')
-print('')
+
 shopping_list = '' 
 
 while True:
-    print('1. Add an item')
+    print('\n1. Add an item')
     print('2. Print currently added items')
-    print('3. Finished')
+    print('3. Finished\n')
 
     choice = int(input('Enter your choice: '))
+    
     if choice == 1:
         item = input('Enter an item: ')
         quantity = int(input('Enter quantity:')) #changed from float to int
@@ -23,14 +24,12 @@ while True:
         print('-'*20)
         print(title.center(20))
         print('-'*20)
-        #if shopping_list=='':
-        #    print('no items ')
-        #else:
         print(shopping_list)
         break
     
     else:
         print('Error: Invalid choice, please try again.')
+
 
 
 
