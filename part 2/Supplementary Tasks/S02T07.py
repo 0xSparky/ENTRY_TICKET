4ab0a0db11856d32369f8ef1f99ec8dbd891336d
MD. Siam Ahmed <devsiam404@gmail.com>
2025-10-22 02:47:34 +0200

    S02T07.py & S02T08.py added

---
+++ part 2/Supplementary Tasks/S02T07.py
@@ -0,0 +1,98 @@
+#!/bin/python3
+
+# menu function
+def print_menu():
+    print()
+    print('-' * 9, ' Menu ', '-' * 10, sep='')
+    print('a) Add a new test subject')
+    print('s) Show all information')
+    print('r) Read from file')
+    print('w) Write to file')
+    print('V) Validate number')
+    print('q) Quit')
+    print('-' * 25)
+    print()
+
+# adding new test subject  == a)
+def new_test_subject():
+    number = (input('Number :   '))
+    time = int(input('Time   :   '))
+    print()
+    return (number, time)
+
+# write data to file        == w)
+def write_data(test_subjects, filename):
+    with open(filename, 'w+') as f:
+        for number, time in test_subjects:
+            f.write(f'{number}\n{time}\n')
+        
+
+# read data                 == r)
+def read_data(filename):
+    data = [] # empty list to contain tuple
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file.readlines()] # making a temporary list to store single data
+
+        for i in range (0, len(lines), 2):
+            number = lines[i]
+            time = int(lines[i + 1])
+            data.append((number,time))
+    return data
+
+# print test subjects       == s)
+def print_test_subjects(test_subjects):
+    print('-' * 4, 'Registered test subjects', '-' * 4, sep=' ')
+    if test_subjects != []:
+        for i,j in test_subjects:
+            print(f"Number :{i:>5}")
+            print(f"Time   :{j:>5}")
+            print()
+
+
+# validate 
+def validate(number):
+    if number >= 1 and number <= 100 and number % 2 == 1:
+        return True
+    else:
+        return False
+
+
+
+def main():
+    test_subjects = [] # empty list
+    while True:
+        print_menu()
+        
+        choice = input('Your choice: ')
+
+        if choice == 'a':
+            test_subjects.append(new_test_subject())
+
+        elif choice == 's':
+            print_test_subjects(test_subjects)
+
+        elif choice == 'r':
+            filename = input('Filename: ')
+            test_subjects = read_data(filename)
+            print()
+            print('*' * 3, 'Data read from file', '*' * 3, sep=' ')
+
+        elif choice == 'w':
+            filename = input('Filename: ')
+            write_data(test_subjects, filename)
+
+        elif choice == 'V':
+            number = int(input('Number: '))
+            if validate(number):
+                print('The number is valid.')
+            else:
+                print('The number is NOT valid.')
+            
+        elif choice == 'q':
+            break
+        else:
+            continue
+
+
+if __name__ == '__main__':
+    main()
---
+++ part 2/Supplementary Tasks/S02T08.py
@@ -0,0 +1,102 @@
+#!/bin/python3
+
+# menu function
+def print_menu():
+    print()
+    print('-' * 9, ' Menu ', '-' * 10, sep='')
+    print('a) Add a new test subject')
+    print('s) Show all information')
+    print('r) Read from file')
+    print('w) Write to file')
+    print('T) Test subjects in range')
+    print('q) Quit')
+    print('-' * 25)
+    print()
+
+# adding new test subject  == a)
+def new_test_subject():
+    number = (input('Number :   '))
+    time = int(input('Time   :   '))
+    print()
+    return (number, time)
+
+# write data to file        == w)
+def write_data(test_subjects, filename):
+    with open(filename, 'w+') as f:
+        for number, time in test_subjects:
+            f.write(f'{number}\n{time}\n')
+        
+
+# read data                 == r)
+def read_data(filename):
+    data = [] # empty list to contain tuple
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file.readlines()] # making a temporary list to store single data
+
+        for i in range (0, len(lines), 2):
+            number = lines[i]
+            time = int(lines[i + 1])
+            data.append((number,time))
+    return data
+
+# print test subjects       == s)
+def print_test_subjects(test_subjects):
+    print('-' * 4, 'Registered test subjects', '-' * 4, sep=' ')
+    if test_subjects != []:
+        for i,j in test_subjects:
+            print(f"Number :{i:>5}")
+            print(f"Time   :{j:>5}")
+            print()
+
+# filter_range
+def filter_range(subjects, min, max):
+    final_list = [] # empty list to store after filtering
+
+    for number, time in subjects:
+        if time >= min and time <= max:
+            final_list.append(number)
+
+    return final_list
+
+
+
+def main():
+    test_subjects = [] # empty list
+    while True:
+        print_menu()
+        
+        choice = input('Your choice: ')
+
+        if choice == 'a':
+            test_subjects.append(new_test_subject())
+
+        elif choice == 's':
+            print_test_subjects(test_subjects)
+
+        elif choice == 'r':
+            filename = input('Filename: ')
+            test_subjects = read_data(filename)
+            print()
+            print('*' * 3, 'Data read from file', '*' * 3, sep=' ')
+
+        elif choice == 'w':
+            filename = input('Filename: ')
+            write_data(test_subjects, filename)
+
+        elif choice == 'T':
+            min = int(input("Min: "))
+            max = int(input("Max: "))
+
+            filtered_list = filter_range(test_subjects, min, max)
+            for i in filtered_list:
+                print(i)
+
+            
+        elif choice == 'q':
+            break
+        else:
+            continue
+
+
+if __name__ == '__main__':
+    main()
